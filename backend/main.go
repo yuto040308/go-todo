cc0c1ff3c61e130d664d66409f4faaf23eb17902
yuto.onishi <yuto.ge6@gmail.com>
2026-04-25 21:25:53 +0900

    goをホットリロードにするように修正

--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello, World!"})
+		c.JSON(200, gin.H{"message": "Hello, World!!!!!!!!!"})
 	})
 	r.Run(":8080")
 }
