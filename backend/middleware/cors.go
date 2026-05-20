1008be7b5b1fc8e66737d4f70b3e9e7ff69f299f
yuto.onishi <yuto.ge6@gmail.com>
2026-05-20 21:58:47 +0900

    フロントのURLを環境変数からセットするように変更

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -1,18 +1,25 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
+	origins := []string{
+		"http://localhost",
+		"http://localhost:3000",
+		"http://127.0.0.1:3000",
+	}
+
+	if frontendURL := os.Getenv("FRONTEND_URL"); frontendURL != "" {
+		origins = append(origins, frontendURL)
+	}
+
 	return cors.New(cors.Config{
-		AllowOrigins: []string{
-			"http://localhost",
-			"http://localhost:3000",
-			"http://127.0.0.1:3000",
-			"https://go-todo-neon.vercel.app",
-		},
+		AllowOrigins:     origins,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
